b45b7e93bf2a5fbe6ca68132b0dc4c443ac7dbb8
Hackbright Student <info@hackbrightacademy.com>
2015-01-19 17:28:20 -0800

    WOO PRACTICING

--- guess.py
+++ guess.py
@@ -13,13 +13,20 @@ def guessinggame():
             guessed_number = int(raw_input("What is your guess? "))
             if guessed_number == number:
                 print "YAY! Congratulations"
-                break
+                play_again = raw_input("Would you like to play again? ")
+                if play_again == 'yes':
+                    print guessed_number
+                else:
+                    break
 
             elif guessed_number > number:
                 print "That is too high. Please try again!"
 
             elif guessed_number < number:
                 print "That is too low. Please try again!"
+
+            elif guessed_number > 11:
+                print "Outta range"
 
         except ValueError:
                 print "That's not a number!"
